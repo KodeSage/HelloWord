26ff4758d88f4ed24bc993a0acadd4403781a809
James Harrison <somtochukwujames2000@gmail.com>
2022-03-07 01:53:16 +0100

    added comments

--- HelloWord.sol
+++ HelloWord.sol
@@ -3,16 +3,16 @@
 pragma solidity >=0.4.25 <0.9.0;
 
 contract HelloWord {
-    string greeting;
+    string greeting; // state variable
 
-constructor( address owner) {
+constructor( address owner) { // default constructor
     owner = msg.sender;
 }
 function setGreeting(string memory _greeting) public {
-    greeting = _greeting;
+    greeting = _greeting; // set greeting to _greeting
     }
 function getGreeting() public view returns (string memory) {
-    return greeting;
+    return greeting;  // return greeting
 }
 
 }
